80b8d0a6274e66e10f6dc1c0a39fbf07b86edf91
nilmar ramos <nilmar-ramos22@hotmail.com>
2016-12-15 20:15:00 -0200

    aulas-completas

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'c0bea9aeac3cc5fda99f4333f93a9287';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
